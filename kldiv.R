8ecdf8fdb4b562d4e45a10b05a438b2a8a090593
Guillaume Basse <gw.basse@gmail.com>
2012-08-01 13:36:38 -0400

    Adds multiple parameters structure

--- kldiv.R
+++ kldiv.R
@@ -1,12 +1,13 @@
 
 
-# Sets the parameters
-u = 1
-s = 2
+
+# --------------  KL Divergence Experiments
+
+get.kldivs <- function (u,s, nl, ns){
 
 # Defines a large and a small sample
-large.sample = exp(rnorm(100000, u, s))
-small.sample = large.sample[seq(1,5000)]
+large.sample = exp(rnorm(nl, u, s))
+small.sample = large.sample[seq(1,ns)]
 
 # Obtains the quantiles for the samples. This allows us to make the density
 # estimation more robust to sampling variations
@@ -33,18 +34,27 @@ control.small.density <- dlnorm(small.sample.density$x, u+1, s+1)
 control.large.density <- dlnorm(large.sample.density$x, u+1, s+1)
 
 # Computes the different kl divergences
-kl.small.th <- sum(th.density * (log(th.small.density) - log(small.sample.density$y)))
-kl.large.th <- sum(th.density * (log(th.large.density) - log(large.sample.density$y)))
+kl.small.th <- sum(th.small.density * (log(th.small.density) - log(small.sample.density$y)))
+kl.large.th <- sum(th.small.density * (log(th.large.density) - log(large.sample.density$y)))
 kl.large.adjusted.th <- sum(th.small.density * (log(th.small.density) - log(large.sample.adjusted.density$y)))
 kl.control.small <- sum(th.small.density * (log(th.small.density) - log(control.small.density)))
 kl.control.large <- sum(th.large.density * (log(th.large.density) - log(control.large.density)))
 
-# Prints the result
-print(kl.small.th)
-print(kl.control.small)
+return(c(kl.small.th, kl.control.small, kl.large.th, kl.control.large, kl.large.adjusted.th))
+}
 
-print(kl.large.th)
-print(kl.control.large)
 
-print(kl.large.adjusted.th)
+# parameters
+u <- seq(1,8)
+s <- c(0.1, 0.5, 1, 5, 10)
+parameters <- expand.grid(u=u,s=s)
 
+
+# Runs the simulations
+res <- apply(parameters, 1, function(x) get.kldivs(x[1],x[2], 100000, 10000))
+res <- cbind(parameters, t(res))
+dt <- data.frame(res)
+colnames(dt) <- c("u","s","ucv","bli","ndr","foo","bar")
+
+# Plots the results
+ggplot(data=dt, aes(x=u, y=s)) + geom_tile(aes(fill=ucv-ndr))
