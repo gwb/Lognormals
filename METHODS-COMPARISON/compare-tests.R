8dfc89399113251a4130e583caa5fffb075eb904
Guillaume Basse <gw.basse@gmail.com>
2012-08-21 13:21:18 -0400

    Adds WU implementation and test

--- METHODS-COMPARISON/compare-tests.R
+++ METHODS-COMPARISON/compare-tests.R
@@ -1,4 +1,4 @@
-
+require(rootSolve)
 
 get.FW <- function(u1, u2, s1, s2){
   sq1 <- s1^2
@@ -148,9 +148,61 @@ AG3P = c(1,1,2,1,1,1,2,2,2,1,1,2,2,2,1,1,2,2,2,1,0,1,1,1,0)
   VZ = ssq1 - G1(mw, ssqw) ^ 2 - 2 * ssq1 / ssqw * G3(mw, ssqw) + G2(mw, ssqw)
   EZ = u1 + G1(mw, ssqw)
 
-  return(c(EZ, VZ))
+  return(c(EZ, sqrt(VZ)))
 }
 
+
+# Tested against Python implementation, works
+get.WU <- function(u1, u2, s1, s2, t1, t2, initv=c(0,1), maxiter=1000){
+  get.npsi <- function(s){
+    xs = c(0.24534,
+          0.73747,
+          1.23407,
+          1.73853,
+          2.25497,
+          2.78880,
+          3.34785,
+          3.94476,
+          4.60369,
+          5.38748)
+    ws = c(4.62243 * 10^(-1),
+          2.86675 * 10^(-1),
+          1.09017 * 10^(-1),
+          2.48105 * 10^(-2),
+          3.24377 * 10^(-3),
+          2.28338 * 10^(-4),
+          7.80255 * 10^(-6),
+          1.08606 * 10^(-7),
+          4.39934 * 10^(-10),
+          2.22939 * 10^(-13))
+    xs <- c(xs, -xs)
+    ws <- c(ws, ws)
+    fn <- function(mu, ssq){
+      return(sum( ws / sqrt(pi) * exp( -s * exp( sqrt( 2*ssq ) * xs + mu ) ) ))
+    }
+    return(fn)
+  }
+  
+  get.nfit <- function(nu1, nu2, ns1, ns2, nt1, nt2){
+    mgfi1 <- get.npsi(nt1)
+    mgfi2 <- get.npsi(nt2)
+
+    mgfi1s <- mgfi1(nu1, ns1^2) * mgfi1(nu2, ns2^2)
+    mgfi2s <- mgfi2(nu1, ns1^2) * mgfi2(nu2, ns2^2)
+
+    fn <- function(x){
+      mu <- x[1]
+      s <- x[2]
+      ssq <- s^2
+      return(c(mgfi1(mu, ssq) - mgfi1s, mgfi2(mu, ssq) - mgfi2s))
+    }
+    return(fn)
+  }
+
+  f <- get.nfit(u1, u2, s1, s2, t1, t2)
+  return( multiroot(f, initv, maxiter=maxiter) )
+  
+}
 
 
 test.FW <- function(u1, u2, s1, s2){
